3d2308f5ff66ca959fab7137714f3acc565ad116
Jeff Small <Jeff@JeffsMacBook.local>
2018-04-21 15:56:26 -0700

    Implemented the collection view data source and delegate for the question view controller

--- GetSmart/ViewControllers/QuestionViewController.swift
+++ GetSmart/ViewControllers/QuestionViewController.swift
@@ -8,28 +8,117 @@
 
 import UIKit
 
-class QuestionViewController: UIViewController {
+class QuestionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    @IBOutlet weak var answerCollectionView: UICollectionView!
+    @IBOutlet weak var questionTextView: UITextView!
+
+    var question: Question?
+    var answers = [Answer]()
+    var selectedAnswer: Answer?
+
+    var cells = [Int: AnswerCollectionViewCell]()
+
+    var questionManager: QuestionManager?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureCollectionView(answerCollectionView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func configureCollectionView(_ collectionView: UICollectionView) {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        answerCollectionView.collectionViewLayout = layout
+        answerCollectionView.dataSource = self
+        answerCollectionView.delegate = self
     }
-    
 
-    /*
-    // MARK: - Navigation
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        guard let questionManager = questionManager else {
+            assertionFailure("Question should not be nil.")
+            return
+        }
+
+        let currentQuestion = questionManager.dataSource?.question(at: questionManager.currentIndex)
+        question = currentQuestion
+        answers = currentQuestion?.answers.shuffled() ?? []
+        navigationItem.title = currentQuestion?.prompt
+        questionTextView.text = currentQuestion?.prompt ?? ""
     }
-    */
 
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let padding: CGFloat = 14
+        let collectionViewWidth = collectionView.frame.width - padding
+        return CGSize(width: collectionViewWidth / 2, height: 100)
+    }
+
+    @IBAction func didTapNextQuestion(_ sender: Any) {
+        guard let questionManager = questionManager, let question = question, let answer = selectedAnswer else {
+//            assertionFailure("Something went wrong.")
+            return
+        }
+        questionManager.answerQuestion(question, answer: answer)
+        let finalIndex = (questionManager.dataSource?.numberOfQuestions() ?? 0) - 1
+        if questionManager.currentIndex >= finalIndex {
+            navigateToResults()
+        } else {
+            navigateToNextQuestion()
+        }
+    }
+
+    func navigateToNextQuestion() {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle(for: QuestionViewController.self))
+        guard let nextQuestionViewController = storyboard.instantiateViewController(withIdentifier: String(describing: QuestionViewController.self)) as? QuestionViewController else {
+            assertionFailure("Could not locate the next question view controller.")
+            return
+        }
+
+        nextQuestionViewController.questionManager = questionManager
+        navigationController?.pushViewController(nextQuestionViewController, animated: true)
+    }
+
+    func navigateToResults() {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle(for: GameResultsViewController.self))
+        guard let resultsViewController = storyboard.instantiateViewController(withIdentifier: String(describing: GameResultsViewController.self)) as? GameResultsViewController else {
+            assertionFailure("Could not locate the game results view controller.")
+            return
+        }
+
+        resultsViewController.questionManager = questionManager
+        navigationController?.pushViewController(resultsViewController, animated: true)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return answers.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let index = indexPath.item
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: AnswerCollectionViewCell.self), for: indexPath) as? AnswerCollectionViewCell,
+            index < answers.count else {
+            assertionFailure("Could not dequeue reusable cell with identifier \(String(describing: AnswerCollectionViewCell.self))")
+            return UICollectionViewCell()
+        }
+
+        cell.configure(from: answers[index])
+        cells[index] = cell
+
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        for cellIndex in 0 ..< cells.count {
+            if cellIndex == indexPath.item {
+                cells[cellIndex]?.select()
+                selectedAnswer = answers[cellIndex]
+            } else {
+                cells[cellIndex]?.deselect()
+            }
+        }
+    }
 }
